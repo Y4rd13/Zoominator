73e5ecbc2757ddeb0ea10f31af94b0403d9dce6d
Benny Serra <benjaserrau@gmail.com>
2020-08-12 03:07:14 -0400

    requirements and re-sorted

--- Zoominator.py
+++ Zoominator.py
@@ -1,6 +1,8 @@
-import os, time, datetime
+import os
+import time
+import datetime
 import pandas as pd
-from modules.zoominator_modules import display_logo, loading_bar, handle_process, P_SUTIL, utils, handle_CSV, check_CSV 
+from modules.zoominator_modules import display_logo, loading_bar, handle_process, P_SUTIL, utils, handle_CSV, check_CSV
 #############################################################################
 
 #############################################################################
@@ -8,44 +10,47 @@ from modules.zoominator_modules import display_logo, loading_bar, handle_process
 loading_bar(r2=100, s=0.009, text='\nCleanning (ctrl+c to stop)...')
 check = False
 while check == False:
-  os.system('cls')
-  display_logo(0)
-  name_input = input('\n> Ingrese nombre y apellido: ').lower().strip().split(' ')
+    os.system('cls')
+    display_logo(0)
+    name_input = input(
+        '\n> Ingrese nombre y apellido: ').lower().strip().split(' ')
 
-  if len(name_input) == 2:
-    name, last_name = name_input[0].capitalize(), name_input[1].capitalize()
-    print(f'\nnombre: {name}, apellido: {last_name}')
-    
-    if name.isalpha() and last_name.isalpha():
-      student_name = (name + ' ' + last_name)
-      
-      if len(student_name) <= 30:
-        print('\n¿Entrar con audio?')
-        audio = input('Y o N: ').lower()
-        print('\n¿Entrar con video?')
-        video = input('Y o N: ').lower()
-        
-        if audio == 'y' or audio == 'n':
-          if video == 'y' or video == 'n':
-            csv_file = check_CSV().check_exist_csv_file('root_csv.txt')
-            #check_CSV().check_csv_content('horario zoom.csv') # !
-            check = True
-          
-          else:
-            print('Ingrese una opción válida')
-            time.sleep(2)
+    if len(name_input) == 2:
+        name, last_name = name_input[0].capitalize(
+        ), name_input[1].capitalize()
+        print(f'\nnombre: {name}, apellido: {last_name}')
+
+        if name.isalpha() and last_name.isalpha():
+            student_name = (name + ' ' + last_name)
+
+            if len(student_name) <= 30:
+                print('\n¿Entrar con audio?')
+                audio = input('Y o N: ').lower()
+                print('\n¿Entrar con video?')
+                video = input('Y o N: ').lower()
+
+                if audio == 'y' or audio == 'n':
+                    if video == 'y' or video == 'n':
+                        csv_file = check_CSV().check_exist_csv_file('root_csv.txt')
+                        # check_CSV().check_csv_content('horario zoom.csv') # !
+                        check = True
+
+                    else:
+                        print('Ingrese una opción válida')
+                        time.sleep(2)
+                else:
+                    print('Ingrese una opción válida')
+                    time.sleep(2)
+            else:
+                print('\nEl largo es mayor a 30 carácteres')
+                time.sleep(3)
         else:
-          print('Ingrese una opción válida')
-          time.sleep(2)
-      else:
-        print('\nEl largo es mayor a 30 carácteres')
-        time.sleep(3)
+            print('\nNombre ingresado no es alfabético')
+            time.sleep(3)
     else:
-      print('\nNombre ingresado no es alfabético')
-      time.sleep(3)
-  else:
-    print(f'\nIngresar nombre y apellido (2 elementos)\nElementos detectados: {len(name_input)}')
-    time.sleep(4)
+        print(
+            f'\nIngresar nombre y apellido (2 elementos)\nElementos detectados: {len(name_input)}')
+        time.sleep(4)
 #############################################################################
 
 # initialize_variables
@@ -53,7 +58,8 @@ user = os.getlogin()
 root = f'C:/Users/{user}/AppData/Roaming/Zoom/bin/'
 file = 'Zoom.exe'
 
-root_csv = str(os.path.dirname(os.path.abspath(__file__))).replace(chr(92), '/') + '/'
+root_csv = str(os.path.dirname(os.path.abspath(__file__))
+               ).replace(chr(92), '/') + '/'
 utils().alert()
 
 # create dict from csv
@@ -63,96 +69,100 @@ registro_clases = handle_CSV(root_csv, csv_file).csv_2_dict()
 TL_registro_clases = utils().dictionary_2_OrderedList(registro_clases)
 check, count, w, main_w = False, 0, 0, True
 while main_w == True:
-  while w == 0:
-    os.system('cls')
-    display_logo(0)
-    print('\n\t1. Modo automático\n\t2. Entrar por id\n\t3. Para salir')
-    try:
-      menu_op = int(input('\n\t> '))
-    except:
-      print('\nError en el input.')
+    while w == 0:
+        os.system('cls')
+        display_logo(0)
+        print('\n\t1. Modo automático\n\t2. Entrar por id\n\t3. Para salir')
+        try:
+            menu_op = int(input('\n\t> '))
+        except:
+            print('\nError en el input.')
 
-    if menu_op == 1:
-      w = 1
-    elif menu_op == 2:
-      w = 2       
-    elif menu_op == 3:
-      os.system('cls')
-      main_w = False
-      w = False
-      break
-    else:
-      print('\nError en el input.')
-      time.sleep(3)
+        if menu_op == 1:
+            w = 1
+        elif menu_op == 2:
+            w = 2
+        elif menu_op == 3:
+            os.system('cls')
+            main_w = False
+            w = False
+            break
+        else:
+            print('\nError en el input.')
+            time.sleep(3)
 
-  while w == 1:
-    os.system('cls')
-    display_logo(0)
-    display_logo(1)
-    print(handle_CSV(root_csv, csv_file).display_csv())
+    while w == 1:
+        os.system('cls')
+        display_logo(0)
+        display_logo(1)
+        print(handle_CSV(root_csv, csv_file).display_csv())
 
-    for ID_class, class_hour in TL_registro_clases:
-      # date variables
-      date = datetime.datetime.now()
-      current_day = date.strftime('%w')
-      current_hour = date.strftime('%X')
+        for ID_class, class_hour in TL_registro_clases:
+            # date variables
+            date = datetime.datetime.now()
+            current_day = date.strftime('%w')
+            current_hour = date.strftime('%X')
 
-      # date to seconds
-      date_sec = utils().get_sec(current_hour, current_day)
-      sunday_sec = utils().get_sec('23:59:59', 7)
+            # date to seconds
+            date_sec = utils().get_sec(current_hour, current_day)
+            sunday_sec = utils().get_sec('23:59:59', 7)
 
-      ###
-      if (class_hour < date_sec) and count < 100:
-        count += 1
-        pass
-      elif count == 100: #(len(TL_registro_clases) * 3)
-        print('Semana finalizada con éxito\n')
-        print('Esperando para el comienzo de la siguiente semana: ')
-        print(date_sec, sunday_sec)
-        loading_bar(r1=date_sec, r2=sunday_sec, text='')
-        time.sleep(1)
-        count = 0
-      else:
-        # cycle
-        print(f'\n' + '#' * 50)
-        print(f'### Barra de progreso para la siguiente clase ###')
-        print('#' * 50)
-        print(date.strftime(f'Hora actual (de inicio): %X,\tFecha %x'))
-        fiveMin_intoSecs = 300
-        loading_bar(r1=date_sec, r2=(class_hour - fiveMin_intoSecs), text=f'ID siguiente clase: {ID_class}')
+            ###
+            if (class_hour < date_sec) and count < 100:
+                count += 1
+                pass
+            elif count == 100:  # (len(TL_registro_clases) * 3)
+                print('Semana finalizada con éxito\n')
+                print('Esperando para el comienzo de la siguiente semana: ')
+                print(date_sec, sunday_sec)
+                loading_bar(r1=date_sec, r2=sunday_sec, text='')
+                time.sleep(1)
+                count = 0
+            else:
+                # cycle
+                print(f'\n' + '#' * 50)
+                print(f'### Barra de progreso para la siguiente clase ###')
+                print('#' * 50)
+                print(date.strftime(f'Hora actual (de inicio): %X,\tFecha %x'))
+                fiveMin_intoSecs = 300
+                loading_bar(r1=date_sec, r2=(class_hour - fiveMin_intoSecs),
+                            text=f'ID siguiente clase: {ID_class}')
 
-        # kill & initialize
-        handle_process(PROCNAME='Zoom.exe').kill()
-        P_SUTIL(audio=audio, video=video).initialize(file, root, student_name, ID_class)
-    
-        # reiniciando
-        loading_bar(r2=30)
-        print(f'\n# R # E # I # N # I # C # I # A # N # D  O #')
-        print(pd.read_csv(root_csv + csv_file))
-        loading_bar(r2=100, s=0.1,text='Preparando siguiente clase...')
+                # kill & initialize
+                handle_process(PROCNAME='Zoom.exe').kill()
+                P_SUTIL(audio=audio, video=video).initialize(
+                    file, root, student_name, ID_class)
 
-  while w == 2:
-    os.system('cls')
-    display_logo(0)
-    display_logo(2)
-    csv_display_1 = handle_CSV(path=root_csv, csv=csv_file).display_csv()
-    len_csv = len(csv_display_1)
-    print(csv_display_1)
-    print('\nSeleccionar por índice la id a la que desea entrar (el primero es 0):')  
-    try:
-      select_id = int(input('\t> '))
-    except:
-      print('Opción inválida.')
+                # reiniciando
+                loading_bar(r2=30)
+                print(f'\n# R # E # I # N # I # C # I # A # N # D  O #')
+                print(pd.read_csv(root_csv + csv_file))
+                loading_bar(r2=100, s=0.1,
+                            text='Preparando siguiente clase...')
 
-    id_count = 0
-    for ID_class, class_hour in TL_registro_clases:
-      if select_id <= (len_csv - 1):
-        if id_count == select_id:
-          # kill & initialize
-          handle_process(PROCNAME='Zoom.exe').kill()
-          P_SUTIL(audio=audio, video=video).initialize(file, root, student_name, ID_class)
-          w = False
-      else:
-        print('\nÍndice fuera de rango.')
-        time.sleep(3)
-      id_count += 1
+    while w == 2:
+        os.system('cls')
+        display_logo(0)
+        display_logo(2)
+        csv_display_1 = handle_CSV(path=root_csv, csv=csv_file).display_csv()
+        len_csv = len(csv_display_1)
+        print(csv_display_1)
+        print('\nSeleccionar por índice la id a la que desea entrar (el primero es 0):')
+        try:
+            select_id = int(input('\t> '))
+        except:
+            print('Opción inválida.')
+
+        id_count = 0
+        for ID_class, class_hour in TL_registro_clases:
+            if select_id <= (len_csv - 1):
+                if id_count == select_id:
+                    # kill & initialize
+                    handle_process(PROCNAME='Zoom.exe').kill()
+                    P_SUTIL(audio=audio, video=video).initialize(
+                        file, root, student_name, ID_class)
+                    w = False
+            else:
+                print('\nÍndice fuera de rango.')
+                time.sleep(3)
+            id_count += 1
--- modules/zoominator_modules.py
+++ modules/zoominator_modules.py
@@ -1,31 +1,35 @@
-import subprocess, os, time, datetime
+import subprocess
+import os
+import time
+import datetime
 from collections import defaultdict
 from tqdm import tqdm
 import os
 
 alert = ['No mover el mouse, ni teclear cuando termine la barra de progreso.',
-        ' De lo contrario, el programa no funcionará correctamente.',
-        '¿Continuar con los procesos?\nEsto no afectará a la clase actual']
+         ' De lo contrario, el programa no funcionará correctamente.',
+         '¿Continuar con los procesos?\nEsto no afectará a la clase actual']
 
 
 try:
-  os.system('color a')
-  os.system('py -m pip install --upgrade pip')
-  os.system('py -m pip install pandas')
-  os.system('py -m pip install tqdm')
-  os.system('py -m pip install pyautogui')
-  os.system('py -m pip install psutil')
+    os.system('color a')
+    os.system('py -m pip install --upgrade pip')
+    os.system('py -m pip install -r requirements')
+#    os.system('py -m pip install tqdm')
+#    os.system('py -m pip install pyautogui')
+#    os.system('py -m pip install psutil')
 except ImportError:
-  print('\n> Error, Module ModuleName is required')
-  raise IOError('\nCannot find: path or modules')
+    print('\n> Error, Module ModuleName is required')
+    raise IOError('\nCannot find: path or modules')
 finally:
-  import pandas as pd
-  import pyautogui as gui 
-  import psutil
+    import pandas as pd
+    import pyautogui as gui
+    import psutil
+
 
 def display_logo(x):
-  if x == 0:
-    print('''
+    if x == 0:
+        print('''
     ███████╗ ██████╗  ██████╗ ███╗   ███╗██╗███╗   ██╗ █████╗ ████████╗ ██████╗ ██████╗ 
     ╚══███╔╝██╔═══██╗██╔═══██╗████╗ ████║██║████╗  ██║██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗
       ███╔╝ ██║   ██║██║   ██║██╔████╔██║██║██╔██╗ ██║███████║   ██║   ██║   ██║██████╔╝
@@ -34,8 +38,8 @@ def display_logo(x):
     ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝                                                                                  
     -------------------------------------------------------------------------------------
     ''')
-  elif x == 1:
-    print('''
+    elif x == 1:
+        print('''
      █████╗ ██╗   ██╗████████╗ ██████╗ ███╗   ███╗ █████╗ ████████╗██╗ ██████╗
     ██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗████╗ ████║██╔══██╗╚══██╔══╝██║██╔════╝
     ███████║██║   ██║   ██║   ██║   ██║██╔████╔██║███████║   ██║   ██║██║     
@@ -44,8 +48,8 @@ def display_logo(x):
     ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝
     ------------------------------------------------------------------------------
     ''')
-  elif x == 2:
-    print('''
+    elif x == 2:
+        print('''
     ███╗   ███╗ █████╗ ███╗   ██╗██╗   ██╗ █████╗ ██╗     
     ████╗ ████║██╔══██╗████╗  ██║██║   ██║██╔══██╗██║     
     ██╔████╔██║███████║██╔██╗ ██║██║   ██║███████║██║     
@@ -55,123 +59,134 @@ def display_logo(x):
     ----------------------------------------------------------                                                  
     ''')
 
-def loading_bar(r2, r1=0, s=1,text='\nWaiting for process...'):
-  print(text)
-  for __ in tqdm(range(r1, r2)):
-    time.sleep(s)
+
+def loading_bar(r2, r1=0, s=1, text='\nWaiting for process...'):
+    print(text)
+    for __ in tqdm(range(r1, r2)):
+        time.sleep(s)
+
 
 class handle_process:
     def __init__(self, PROCNAME):
         self.PROCNAME = PROCNAME
 
     def kill(self):
-      print(f'\nKill process: {self.PROCNAME}')
-      for proc in psutil.process_iter():
-        try:
-          if proc.name().lower() == self.PROCNAME.lower():
-            proc.kill()
-            print('Killed: ', True)
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-          print('Killed: ', False)
-          os.system("taskkill /f /im Zoom.exe")
-      loading_bar(r2=100, s=0.01, text=f'Killing process... {self.PROCNAME}')
+        print(f'\nKill process: {self.PROCNAME}')
+        for proc in psutil.process_iter():
+            try:
+                if proc.name().lower() == self.PROCNAME.lower():
+                    proc.kill()
+                    print('Killed: ', True)
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                print('Killed: ', False)
+                os.system("taskkill /f /im Zoom.exe")
+        loading_bar(r2=100, s=0.01, text=f'Killing process... {self.PROCNAME}')
 
     def check(self):
-      boolean_check_process = []
-      for proc in psutil.process_iter():
-        try:
-          if proc.name().lower() == self.PROCNAME.lower():
-            boolean_check_process.append(True)
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-          boolean_check_process.append(False)
-      return boolean_check_process
+        boolean_check_process = []
+        for proc in psutil.process_iter():
+            try:
+                if proc.name().lower() == self.PROCNAME.lower():
+                    boolean_check_process.append(True)
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                boolean_check_process.append(False)
+        return boolean_check_process
+
 
 class P_SUTIL:
     def __init__(self, audio, video):
         self.audio = audio
         self.video = video
+
     def audio_video(self):
-      if self.audio == 'y' and self.video == 'y':
-        gui.press(['tab'], presses = 2) 
-        gui.press(['tab'])
-      elif self.audio == 'y' and self.video == 'n':
-        gui.press(['tab'], presses = 2)    
-        gui.press(['tab', 'enter']) 
-      elif self.audio == 'n' and self.video == 'y':
-        gui.press(['tab'], presses = 2)
-        gui.press(['enter'])
-        gui.press(['tab'])
-      elif self.audio == 'n' and self.video == 'n':
-        gui.press(['tab'], presses = 2) # sin audio
-        gui.press(['enter']) # sin audio
-        gui.press(['tab', 'enter']) # sin video
+        if self.audio == 'y' and self.video == 'y':
+            gui.press(['tab'], presses=2)
+            gui.press(['tab'])
+        elif self.audio == 'y' and self.video == 'n':
+            gui.press(['tab'], presses=2)
+            gui.press(['tab', 'enter'])
+        elif self.audio == 'n' and self.video == 'y':
+            gui.press(['tab'], presses=2)
+            gui.press(['enter'])
+            gui.press(['tab'])
+        elif self.audio == 'n' and self.video == 'n':
+            gui.press(['tab'], presses=2)  # sin audio
+            gui.press(['enter'])  # sin audio
+            gui.press(['tab', 'enter'])  # sin video
 
     def initialize(self, file, root, student_name, ID_class):
-      posx, posy = gui.size()
-      x, y = int(posx / 2), int(posy / 2)
-      print(f'\nTamaño de la pantalla: {posx}, {posy}\nPunto medio: {x}, {y}')
+        posx, posy = gui.size()
+        x, y = int(posx / 2), int(posy / 2)
+        print(
+            f'\nTamaño de la pantalla: {posx}, {posy}\nPunto medio: {x}, {y}')
 
-      gui.hotkey('win', 'd')  
-      print(f'\nOpening executable: {file}...\n')
-      os.startfile(root + file)
-    
-      checking, counted = False, 1
-      while checking == False and counted != 6:
-        print('La barra de progreso de demorará más dependiendo de la velocidad de su computador.')
-        loading_bar(r2=70 * 1, s=0.1, text=f'Trying to check process {file}, tries: {counted}, waiting: {(70 * counted * 0.1)} sec.')
-        if True in handle_process(file).check():
-          print(f'\nProcess checked as: {handle_process(file).check()}')
-          time.sleep(1)
-          gui.click(x = x, y = y, button = 'left')
-          time.sleep(1)
-          gui.press(['tab', 'enter'])
-          time.sleep(1)
-          gui.write(ID_class)
-          gui.press(['tab'], presses = 2)
-          gui.hotkey('ctrl', 'a')
-          gui.write(student_name)
-          P_SUTIL(self.audio, self.video).audio_video()
-          time.sleep(1)
-          gui.press(['tab', 'enter'])
-          checking = True
-        else:
-          print(f'{file} process is not running.') 
-          print(handle_process(file).check())
-          counted += 1      
+        gui.hotkey('win', 'd')
+        print(f'\nOpening executable: {file}...\n')
+        os.startfile(root + file)
+
+        checking, counted = False, 1
+        while checking == False and counted != 6:
+            print(
+                'La barra de progreso de demorará más dependiendo de la velocidad de su computador.')
+            loading_bar(
+                r2=70 * 1, s=0.1, text=f'Trying to check process {file}, tries: {counted}, waiting: {(70 * counted * 0.1)} sec.')
+            if True in handle_process(file).check():
+                print(f'\nProcess checked as: {handle_process(file).check()}')
+                time.sleep(1)
+                gui.click(x=x, y=y, button='left')
+                time.sleep(1)
+                gui.press(['tab', 'enter'])
+                time.sleep(1)
+                gui.write(ID_class)
+                gui.press(['tab'], presses=2)
+                gui.hotkey('ctrl', 'a')
+                gui.write(student_name)
+                P_SUTIL(self.audio, self.video).audio_video()
+                time.sleep(1)
+                gui.press(['tab', 'enter'])
+                checking = True
+            else:
+                print(f'{file} process is not running.')
+                print(handle_process(file).check())
+                counted += 1
+
 
 class utils:
     def __init__(self):
-        pass    
+        pass
+
     def get_sec(self, time_str, day_str):
-      if len(time_str.split(':')) < 3:
-        time_str = time_str + ':00'
+        if len(time_str.split(':')) < 3:
+            time_str = time_str + ':00'
 
-      h, m, s = time_str.split(':')
-      day_sec, d = 86000, int(day_str)
+        h, m, s = time_str.split(':')
+        day_sec, d = 86000, int(day_str)
 
-      if d == 0:
-        dx = day_sec * 7
-      else:
-        dx = day_sec * d 
-      return  dx - (day_sec - (int(h) * 3600 + int(m) * 60))
+        if d == 0:
+            dx = day_sec * 7
+        else:
+            dx = day_sec * d
+        return dx - (day_sec - (int(h) * 3600 + int(m) * 60))
 
     def hours_left(self, class_sec, date_sec):
-      hours_left = int((int(class_sec) / 3600) - (int(date_sec) / 3600))
-      min_left = ((class_sec - int(class_sec)) * 60) - ((date_sec - int(date_sec)) * 60)
-      return f'{hours_left}:{min_left}'
+        hours_left = int((int(class_sec) / 3600) - (int(date_sec) / 3600))
+        min_left = ((class_sec - int(class_sec)) * 60) - \
+            ((date_sec - int(date_sec)) * 60)
+        return f'{hours_left}:{min_left}'
 
     def dictionary_2_OrderedList(self, d):
-      l = []
-      for key,values in d.items():
-        for value in values:
-          l.append((key,value))
+        l = []
+        for key, values in d.items():
+            for value in values:
+                l.append((key, value))
 
-      l.sort(key=lambda x:x[1])
-      return l
+        l.sort(key=lambda x: x[1])
+        return l
 
     def alert(self):
-      global alert
-      gui.alert(text = alert[0] + alert[1], button = 'Continuar')
+        global alert
+        gui.alert(text=alert[0] + alert[1], button='Continuar')
+
 
 class handle_CSV:
     def __init__(self, path, csv):
@@ -179,72 +194,76 @@ class handle_CSV:
         self.csv = csv
 
     def display_csv(self):
-      path_csv = self.path + self.csv
-      df = pd.read_csv(path_csv)
-      return df
+        path_csv = self.path + self.csv
+        df = pd.read_csv(path_csv)
+        return df
 
     def csv_2_dict(self):
-      path_csv = self.path + self.csv
+        path_csv = self.path + self.csv
 
-      df = pd.read_csv(path_csv, usecols = [0, 1, 2])
-      dd = defaultdict(list)
+        df = pd.read_csv(path_csv, usecols=[0, 1, 2])
+        dd = defaultdict(list)
 
-      idx = [str(i) for i in df['id']]
-      time_str = [str(i) for i in df['hora']]
-      day_str = [str(i) for i in df['dia']]
-      zipped = zip(idx, time_str, day_str)
+        idx = [str(i) for i in df['id']]
+        time_str = [str(i) for i in df['hora']]
+        day_str = [str(i) for i in df['dia']]
+        zipped = zip(idx, time_str, day_str)
 
-      loading_bar(r2=100, s=0.015, text='\nCargando datos del csv: ')
+        loading_bar(r2=100, s=0.015, text='\nCargando datos del csv: ')
 
-      for idx, j, k in zipped:
-        class_hour = utils().get_sec(j, k)
-        dd[idx].append(class_hour)
-      return dd
+        for idx, j, k in zipped:
+            class_hour = utils().get_sec(j, k)
+            dd[idx].append(class_hour)
+        return dd
+
 
 class check_CSV:
     def __init__(self):
         pass
-    def check_exist_csv_file(self, CSV_SAVE_FILENAME):
-      try:
-        x = False
-        if os.path.exists(CSV_SAVE_FILENAME):
-          while x == False:
-            print('\n¿Desea modificar el nombre del archivo.csv? Y o N')
-            op_csv = input('> ').lower()
 
-            if op_csv == 'y':
-              csv_file = input('\nIngrese SuArchivo.csv: ') 
-              with open(CSV_SAVE_FILENAME, 'w') as f:
-                f.write(csv_file)
-              print('\ncambiado exitosamente...')
-              x = True
-            elif op_csv == 'n':
-              x = True
+    def check_exist_csv_file(self, CSV_SAVE_FILENAME):
+        try:
+            x = False
+            if os.path.exists(CSV_SAVE_FILENAME):
+                while x == False:
+                    print('\n¿Desea modificar el nombre del archivo.csv? Y o N')
+                    op_csv = input('> ').lower()
+
+                    if op_csv == 'y':
+                        csv_file = input('\nIngrese SuArchivo.csv: ')
+                        with open(CSV_SAVE_FILENAME, 'w') as f:
+                            f.write(csv_file)
+                        print('\ncambiado exitosamente...')
+                        x = True
+                    elif op_csv == 'n':
+                        x = True
+                    else:
+                        print('\nError en la opción ingresada.')
             else:
-              print('\nError en la opción ingresada.')
-        else:
-          while x == False:
-            csv_file = input('\nIngrese nombre del archivo.csv: ').strip()
-            if csv_file.endswith('.csv'):
-              with open(CSV_SAVE_FILENAME, 'w') as f:
-                f.write(csv_file)
-              x = True
-            else:
-              print('Debe ingresar formato .csv, ejemplo: archivo.csv')
-      except:
-        return False
-      finally:
-        with open(CSV_SAVE_FILENAME, 'r') as f:
-          csv_name = f.readline()
-        return csv_name
+                while x == False:
+                    csv_file = input(
+                        '\nIngrese nombre del archivo.csv: ').strip()
+                    if csv_file.endswith('.csv'):
+                        with open(CSV_SAVE_FILENAME, 'w') as f:
+                            f.write(csv_file)
+                        x = True
+                    else:
+                        print('Debe ingresar formato .csv, ejemplo: archivo.csv')
+        except:
+            return False
+        finally:
+            with open(CSV_SAVE_FILENAME, 'r') as f:
+                csv_name = f.readline()
+            return csv_name
 
     def check_csv_content(self, CSV_FILENAME):
-      df =  (pd.read_csv(CSV_FILENAME))
-      validar = '#BORRAR ESTA LINEA PARA VALIDAR ARCHIVO'
-      if validar in df:
-        print('validar')
-        os.startfile(CSV_FILENAME)
-        gui.alert(text = f'Por favor, hacer válido su {CSV_FILENAME} csv antes de continuar', button = 'Acepto')
+        df = (pd.read_csv(CSV_FILENAME))
+        validar = '#BORRAR ESTA LINEA PARA VALIDAR ARCHIVO'
+        if validar in df:
+            print('validar')
+            os.startfile(CSV_FILENAME)
+            gui.alert(
+                text=f'Por favor, hacer válido su {CSV_FILENAME} csv antes de continuar', button='Acepto')
 
 
 '''
